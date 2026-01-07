3a653a80b2a0287292975b450f2e52a8fc505a0a
luckygurrala2205 <luckygurrala2205@gmail.com>
2026-01-07 18:40:52 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const aid=143553
+let aemail="ak@"
+var apass="1234"
+//aid=2 not allowed
+acity="hyd"
+aemail="ch@"
+apass="1234"
+acity="bang"
+/*
+prefer not to use var
+*/
+console.log(aemail)
+console.table([aid,aemail,apass,acity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"// treat all js code as newer version
+//alert(3+3) does not works in node js as it is browser
+//code readability should be high
+let name="ak"
+let age=18
+let islogged=false
+//number
+//bigint
+//boolean==true/false
+//null==standalone value
+//undefined
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
